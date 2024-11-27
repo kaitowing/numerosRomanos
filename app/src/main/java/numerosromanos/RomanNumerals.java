4969304d1118d66f9ef4be0c3c6553cd9fd40509
Kaitowing <pizzolatti.arthur@edu.pucrs.com.br>
2024-11-27 13:34:58 -0300

    Refactor Roman numeral conversion logic for improved readability and maintainability

--- app/src/main/java/numerosromanos/RomanNumerals.java
+++ app/src/main/java/numerosromanos/RomanNumerals.java
@@ -11,143 +11,83 @@ public class RomanNumerals {
 
     private String convertEntryToRoman(int decimalValue) {
         String romanRepresentation = "";
-        int currentDivider = 1000;
-        int quotient;
-        int dividerStep = 6;
-
-        do {
-            quotient = decimalValue / currentDivider;
-            switch (currentDivider) {
-                case 1000:
-                    if (quotient == 0) {
-                        if (decimalValue >= 900) {
-                            romanRepresentation += "CM";
-                            decimalValue -= 900;
-                        }
-                    } else {
-                        if (decimalValue > 500) {
-                            dividerStep++;
-                        }
-                    }
-                    for (int i = 0; i < quotient; i++) {
-                        romanRepresentation += "M";
-                    }
-                    break;
-                case 500:
-                    if (quotient == 0) {
-                        if (decimalValue >= 400) {
-                            romanRepresentation += "CD";
-                            decimalValue -= 400;
-                        }
-                    } else {
-                        if (decimalValue > 100) {
-                            dividerStep++;
-                        }
-                    }
-
-                    for (int i = 0; i < quotient; i++) {
-                        romanRepresentation += "D";
-                    }
-                    break;
-                case 100:
-                    if (quotient == 0) {
-                        if (decimalValue >= 90) {
-                            romanRepresentation += "XC";
-                            decimalValue -= 90;
-                        }
-                    } else {
-                        if (decimalValue > 50) {
-                            dividerStep++;
-                        }
-                    }
-
-                    for (int i = 0; i < quotient; i++) {
-                        romanRepresentation += "C";
-                    }
-                    break;
-
-                case 50:
-                    if (quotient == 0) {
-                        if (decimalValue >= 40) {
-                            romanRepresentation += "XL";
-                            decimalValue -= 40;
-                        }
-                    } else {
-                        if (decimalValue > 10) {
-                            dividerStep++;
-                        }
-                    }
-
-                    for (int i = 0; i < quotient; i++) {
-                        romanRepresentation += "L";
-                    }
-                    break;
-                case 10:
-                    if (quotient == 0) {
-                        if (decimalValue == 9) {
-                            romanRepresentation += "IX";
-                            return romanRepresentation;
-                        }
-                    } else if (decimalValue > 5) {
-                        dividerStep++;
-                    }
-
-                    for (int i = 0; i < quotient; i++) {
-                        romanRepresentation += "X";
-                    }
-                    break;
-                case 5:
-                    if (quotient == 0) {
-                        if (decimalValue == 4)
-                            romanRepresentation += "IV";
-                        else
-                            for (int i = 0; i < decimalValue; i++) {
-                                romanRepresentation += "I";
-                            }
-
-                        return romanRepresentation;
-                    }
-
-                    for (int i = 0; i < quotient; i++) {
-                        romanRepresentation += "V";
-                    }
-                    break;
-                default:
-                    break;
+        while (true) {
+            if (decimalValue >= 1000) {
+                romanRepresentation += getRomanEquivalent(1000);
+                decimalValue -= 1000;
+            } else if (decimalValue >= 900) {
+                romanRepresentation += getRomanEquivalent(900);
+                decimalValue -= 900;
+            } else if (decimalValue >= 500) {
+                romanRepresentation += getRomanEquivalent(500);
+                decimalValue -= 500;
+            } else if (decimalValue >= 400) {
+                romanRepresentation += getRomanEquivalent(400);
+                decimalValue -= 400;
+            } else if (decimalValue >= 100) {
+                romanRepresentation += getRomanEquivalent(100);
+                decimalValue -= 100;
+            } else if (decimalValue >= 90) {
+                romanRepresentation += getRomanEquivalent(90);
+                decimalValue -= 90;
+            } else if (decimalValue >= 50) {
+                romanRepresentation += getRomanEquivalent(50);
+                decimalValue -= 50;
+            } else if (decimalValue >= 40) {
+                romanRepresentation += getRomanEquivalent(40);
+                decimalValue -= 40;
+            } else if (decimalValue >= 10) {
+                romanRepresentation += getRomanEquivalent(10);
+                decimalValue -= 10;
+            } else if (decimalValue >= 9) {
+                romanRepresentation += getRomanEquivalent(9);
+                decimalValue -= 9;
+            } else if (decimalValue >= 5) {
+                romanRepresentation += getRomanEquivalent(5);
+                decimalValue -= 5;
+            } else if (decimalValue >= 4) {
+                romanRepresentation += getRomanEquivalent(4);
+                decimalValue -= 4;
+            } else if (decimalValue >= 1) {
+                romanRepresentation += getRomanEquivalent(1);
+                decimalValue -= 1;
+            } else {
+                return romanRepresentation;
             }
+        }
+    }
 
-            decimalValue = decimalValue % currentDivider;
-            dividerStep--;
-
-            switch (dividerStep) {
-                case 1:
-                    currentDivider = 5;
-                    break;
-
-                case 2:
-                    currentDivider = 10;
-                    break;
-
-                case 3:
-                    currentDivider = 50;
-                    break;
-
-                case 4:
-                    currentDivider = 100;
-                    break;
-
-                case 5:
-                    currentDivider = 500;
-                    break;
-                case 6:
-                    currentDivider = 1000;
-                    break;
-                default:
-                    break;
-            }
-        } while (decimalValue > 0);
-
-        return romanRepresentation;
+    private String getRomanEquivalent(int decimalValue) {
+        switch (decimalValue) {
+            case 1:
+                return "I";
+            case 4:
+                return "IV";
+            case 5:
+                return "V";
+            case 9:
+                return "IX";
+            case 10:
+                return "X";
+            case 40:
+                return "XL";
+            case 50:
+                return "L";
+            case 90:
+                return "XC";
+            case 100:
+                return "C";
+            case 400:
+                return "CD";
+            case 500:
+                return "D";
+            case 900:
+                return "CM";
+            case 1000:
+                return "M";
+            default:
+                return "";
+        }
     }
 
     private boolean checkValidEntry(int decimalValue) {
